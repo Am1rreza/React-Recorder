4cb1405eab70010ca44ea85f963f34741ab5c7cb
Amirreza <amirreza.m13825@gmail.com>
2023-05-04 01:34:54 +0430

    add the Webcam component and it's options

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import Webcam from "react-webcam";
 import "./App.css";
 
+const videoConstraints = {
+  width: { min: 480 },
+  height: { min: 720 },
+};
+
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Webcam
+        audio={true}
+        width={480}
+        height={720}
+        videoConstraints={videoConstraints}
+      />
+    </div>
+  );
 }
 
 export default App;
