91a1a5a9697fbb056d677e1f30d772621d3297fe
Amirreza <amirreza.m13825@gmail.com>
2023-05-04 02:08:03 +0430

    remove the WebcamVideo component and move it's content to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,80 @@
-import Webcam from "react-webcam";
 import "./App.css";
-
-const videoConstraints = {
-  width: { min: 480 },
-  height: { min: 720 },
-};
+import Webcam from "react-webcam";
+import { useCallback, useRef, useState } from "react";
 
 function App() {
+  const webcamRef = useRef(null);
+  const mediaRecorderRef = useRef(null);
+  const [capturing, setCapturing] = useState(false);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+
+  const handleDataAvailable = useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStartCaptureClick = useCallback(() => {
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm",
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
+
+  const handleStopCaptureClick = useCallback(() => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  }, [mediaRecorderRef, setCapturing]);
+
+  const handleDownload = useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+      a.click();
+      window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
+  const videoConstraints = {
+    width: { min: 480 },
+    height: { min: 720 },
+  };
+
   return (
     <div className="App">
-      <Webcam
-        audio={true}
-        width={480}
-        height={720}
-        videoConstraints={videoConstraints}
-      />
+      <div className="Container">
+        <Webcam
+          audio={true}
+          width={480}
+          height={720}
+          videoConstraints={videoConstraints}
+          ref={webcamRef}
+        />
+        {capturing ? (
+          <button onClick={handleStopCaptureClick}>Stop Capture</button>
+        ) : (
+          <button onClick={handleStartCaptureClick}>Start Capture</button>
+        )}
+        {recordedChunks.length > 0 && (
+          <button onClick={handleDownload}>Download</button>
+        )}
+      </div>
     </div>
   );
 }
